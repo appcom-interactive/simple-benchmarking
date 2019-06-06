1964e9c27a39b55fe8d37d485b669beadd620686
Stefan Neidig <s.neidig@appcom-interactive.de>
2019-06-06 16:01:11 +0200

    Fix issues regarding style guide

--- benchmark.js
+++ benchmark.js
@@ -8,20 +8,20 @@ const asyncQ = require('async-q');
 
 class Benchmark {
   static async executeSuite(testCases, args) {
-    console.log(`Executing test suite '${args.name}'`);
+    console.info(`Executing test suite '${args.name}'`);
 
     const testCaseNames = lodash.keys(testCases);
     const startTime = process.hrtime();
 
     let processes = await asyncQ.eachSeries(testCaseNames.map(name => ({
       method: testCases[name].method,
-      rounds: testCases[name].rounds | args.rounds,
-      name: `${args.name || 'Unamed suite'}: ${name}`,
+      rounds: testCases[name].rounds || args.rounds,
+      name: `${args.name || 'Unamed suite'}: ${name}`
     })), Benchmark.execute);
 
     const endTime = process.hrtime(startTime);
     Benchmark.displayExecutionTime(endTime, 'Total execution time of test suite');
-    console.log('');
+    console.info('');
 
     if (args.sort) {
       processes = lodash.sortBy(processes, ['average', 'total', 'name']).reverse();
@@ -32,15 +32,15 @@ class Benchmark {
 
   static displayExecutionTimeObjects(processes) {
     processes.forEach((proc, index) => {
-      console.log(`${index + 1}.\n`);
-      console.log(`Executed test case: '${proc.name}' in ${proc.rounds} rounds`);
+      console.info(`${index + 1}.\n`);
+      console.info(`Executed test case: '${proc.name}' in ${proc.rounds} rounds`);
       Benchmark.displayExecutionTime(proc.total, 'Total execution time');
       Benchmark.displayExecutionTime(proc.average, 'Average execution time');
-      console.log('');
+      console.info('');
     });
   }
 
-  static async execute({ method, name = '', rounds = 100000, verbose = false } = {}) {
+  static async execute({ method, name = '', rounds = 100000 } = {}) { // eslint-disable-line
     const startTime = process.hrtime();
 
     const array = lodash.times(rounds, () => method);
@@ -56,18 +56,18 @@ class Benchmark {
     };
   }
 
-  static async measureExecution(method, args) {
+  static async measureExecution(method) {
     const startTime = process.hrtime();
     try {
       (await method);
-    } catch (error) {
+    } catch (error) { // eslint-disable-line
     }
     return process.hrtime(startTime);
-  };
+  }
 
   static displayExecutionTime(time, title = 'Execution time:') {
-    console.log(`${title}: ${time[0]}s ${(time[1] / 1000000)}ms`);
-  };
+    console.info(`${title}: ${time[0]}s ${(time[1] / 1000000)}ms`);
+  }
 
   static calculateAverageExecutionTime(executionTimesArray) {
     if (executionTimesArray.length === 0) {
@@ -75,12 +75,11 @@ class Benchmark {
     }
 
     const result = executionTimesArray.reduce((a, b) => [a[0] + b[0], a[1] + b[1]], [0, 0]);
-    const totalElapsedTime = result[0] + result[1] / 1000000000;
+    const totalElapsedTime = result[0] + (result[1] / 1000000000);
     const averageElapsedTime = totalElapsedTime / executionTimesArray.length;
 
     return [Math.trunc(averageElapsedTime), (averageElapsedTime - Math.floor(averageElapsedTime)) * 1000000000];
-  };
-
+  }
 }
 
 module.exports = Benchmark;
